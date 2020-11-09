c9341c9b66d47ff16125f67f4dfe7ede9e79c42e
Gerson <gersonjorge6@gmail.com>
2020-11-08 23:40:00 -0500

    mi primer commit

---
+++ adivina_el_numero.py
@@ -0,0 +1,47 @@
+import random
+
+
+def run():
+    resp='si'
+    while(resp!='no'):
+    
+        intentos=1
+    
+        nmr_aleatorio=random.randint(1,100)
+        nmr_elegido=int(input("elige un numero del 1 al 100: \n"))
+    
+        while(nmr_elegido!=nmr_aleatorio ):
+        
+        
+            if(nmr_elegido<1 or nmr_elegido>100):
+               print("ERROOORR\n NUMERO INGRESADO NO ESTA EN EL RANGO DEL 1 AL 100")
+               break
+            else:
+                 if(nmr_elegido<nmr_aleatorio):
+                    print("\ningrese un numero mas mayor!")
+                 else:
+                    print("\ningrese un numero mas pequeño!")
+            
+                 nmr_elegido=int(input("\ningrese denuevo otro numero: "))
+                 intentos-=1
+                 print("te quedan ",intentos," intentos.")
+                 if(intentos==0):
+                    print("GAME OVERR :,(")
+                    print("el numero aleatorio era: ",nmr_aleatorio)
+                    break
+           
+        if(nmr_elegido==nmr_aleatorio):
+           print("Felicidades ganaste te quedaron: ",intentos," intentos")
+        else:
+            print("error")
+        print("desea continuar <si/no>")
+        resp=input()
+        
+    
+   
+    
+      
+
+
+if __name__=='__main__':
+    run()
