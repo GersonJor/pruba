a4977f8e29829907b55daf370e1e054ec1967e92
Gerson <gersonjorge6@gmail.com>
2020-11-08 23:58:52 -0500

    el numero de intentos tiene 5 :V

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -5,7 +5,7 @@ def run():
     resp='si'
     while(resp!='no'):
     
-        intentos=1
+        intentos=5
     
         nmr_aleatorio=random.randint(1,100)
         nmr_elegido=int(input("elige un numero del 1 al 100: \n"))
